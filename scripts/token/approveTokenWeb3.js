ba8bca196ed359e04955102e42dda9ba97aa87f0
opcc <opcc22059@gmail.com>
2022-05-17 22:33:45 +0800

    update

---
+++ scripts/token/approveTokenWeb3.js
@@ -0,0 +1,130 @@
+const {ethers} = require("hardhat");
+
+const BigNumber = require('bignumber.js');
+
+const Web3 = require("web3");
+const secret = require('../../.secret.js');
+const pk = secret.pk;
+
+const config = require("../../hardhat.config.js");
+const contracts = require('../deployed.js');
+
+const tokenABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+]
+
+const v = process.argv
+const net = process.env.HARDHAT_NETWORK
+
+const rpc = config.networks[net].url
+var web3 = new Web3(new Web3.providers.HttpProvider(rpc));
+
+//Example: HARDHAT_NETWORK='bsc' node approveTokenWeb3.js testA 0x3a6d8ca21d1cf76f653a67577fa0d27453350dd8
+
+const para = {
+    tokenSymbol: v[2],
+    tokenAddress: contracts[net][v[2]],
+    approveAddress: v[3],
+}
+
+async function main() {
+
+
+  console.log("Paramters: ");
+  for ( var i in para) { console.log("    " + i + ": " + para[i]); }
+  
+  const [deployer] = await ethers.getSigners();
+  var token = new web3.eth.Contract(tokenABI, para.tokenAddress);
+
+  const txData = await token.methods.approve(para.approveAddress, '1000000000000000000000000000000').encodeABI()
+  const gasLimit = await token.methods.approve(para.approveAddress, '1000000000000000000000000000000').estimateGas({from: deployer.address});
+  console.log('gas limit: ', gasLimit);
+  const signedTx = await web3.eth.accounts.signTransaction(
+      {
+          // nonce: 0,
+          to: para.tokenAddress,
+          data:txData,
+          gas: BigNumber(gasLimit * 1.1).toFixed(0, 2),
+          gasPrice: 5000000000,
+      }, 
+      pk
+  );
+  // nonce += 1;
+  const tx = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  console.log('tx: ', tx);
+  
+}
+
+main().then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+})
+
+module.exports = main;
