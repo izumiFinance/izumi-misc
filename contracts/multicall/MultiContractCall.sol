cf8fe8fa7cc222a6e044a47665032e333f19b15b
opcc <opcc22059@gmail.com>
2022-07-04 16:06:28 +0800

    update

--- contracts/multicall/MultiContractCall.sol
+++ contracts/multicall/MultiContractCall.sol
@@ -6,7 +6,7 @@ contract MultiContractCall {
         successes = new bool[](data.length);
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
-            (bool success, bytes memory result) = contracts[i].delegatecall(data[i]);
+            (bool success, bytes memory result) = contracts[i].call(data[i]);
             successes[i] = success;
             results[i] = result;
         }
