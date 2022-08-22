d411fd856351068883f905f359cf9ec4674baa85
opcc <opcc22059@gmail.com>
2022-08-22 20:28:26 +0800

    update

--- contracts/acl/iZiSwapACL/BscSwapACL.sol
+++ contracts/acl/iZiSwapACL/BscSwapACL.sol
@@ -36,8 +36,8 @@ contract BscSwapAccessControl {
         safeAddress = _safeAddress;
         safeModule = _safeModule;
         
-        // wbnb
-        tokenWhiteList[0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c] = true;
+        // weth
+        tokenWhiteList[0x2170Ed0880ac9A755fd29B2688956BD959F933F8] = true;
         // usdc
         tokenWhiteList[0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d] = true;
     }
