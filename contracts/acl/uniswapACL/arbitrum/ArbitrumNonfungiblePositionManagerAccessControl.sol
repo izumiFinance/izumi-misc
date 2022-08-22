d9c5f95b0c916150d75bc4dc242a1c3235c2511b
opcc <opcc22059@gmail.com>
2022-08-22 21:02:07 +0800

    update

--- contracts/acl/uniswapACL/arbitrum/ArbitrumNonfungiblePositionManagerAccessControl.sol
+++ contracts/acl/uniswapACL/arbitrum/ArbitrumNonfungiblePositionManagerAccessControl.sol
@@ -2,22 +2,86 @@
 
 pragma solidity ^0.8.4;
 
-import "../base/NonfungiblePositionManagerAccessControl.sol";
+import "../../../interfaces/uniswap/INonfungiblePositionManager.sol";
 
 
-contract ArbitrumNonfungiblePositionManagerAccessControl is NonfungiblePositionManagerAccessControl {
+contract ArbitrumNonfungiblePositionManagerAccessControl {
+
+    address public safeModule;
+    address public safeAddress;
+    address public uniV3NFTManager;
+
+    mapping(address=>bool) public tokenWhiteList;
+    
+
+    bytes32 internal _checkedRole;
 
     constructor(
         address _safeAddress,
         address _safeModule
-    ) NonfungiblePositionManagerAccessControl(
-        _safeAddress, 
-        _safeModule,
-        0xC36442b4a4522E871399CD717aBDD847Ab11FE88 // nft address
     ) {
+
+        require(_safeAddress != address(0), "invalid safe address");
+        require(_safeModule!= address(0), "invalid module address");
+
+        safeAddress = _safeAddress;
+        safeModule = _safeModule;
+        // uniswap v3 nft
+        uniV3NFTManager = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
         // usdc
         tokenWhiteList[0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8] = true;
         // weth
         tokenWhiteList[0x82aF49447D8a07e3bd95BD0d56f35241523fBab1] = true;
     }
+    modifier onlySelf() {
+        require(address(this) == msg.sender, "Caller is not inner");
+        _;
+    }
+
+    modifier onlyModule() {
+        require(safeModule == msg.sender, "Caller is not the module");
+        _;
+    }
+
+    function check(bytes32 _role, bytes calldata data) external onlyModule returns (bool) {
+        _checkedRole = _role;
+        (bool success,) = address(this).staticcall(data);
+        return success;
+    }
+
+    fallback() external {
+        revert("Unauthorized access");
+    }
+    function mint(INonfungiblePositionManager.MintParams calldata params) external view onlySelf {
+        require(tokenWhiteList[params.token0], "token0 is not allowed");
+        require(tokenWhiteList[params.token1], "token1 is not allowed");
+        require(params.recipient == safeAddress, "recipient must be safe address");
+        require(params.fee == 500, "feetier must be 0.05%");
+    }
+
+    function increaseLiquidity(INonfungiblePositionManager.IncreaseLiquidityParams calldata params) external view onlySelf {
+
+        require(INonfungiblePositionManager(uniV3NFTManager).ownerOf(params.tokenId) == safeAddress, "nft owner must be safe address");
+    }
+
+    function decreaseLiquidity(INonfungiblePositionManager.DecreaseLiquidityParams calldata params) external view onlySelf {
+
+    }
+
+    function collect(INonfungiblePositionManager.CollectParams calldata params) external view onlySelf {
+        require(params.recipient == safeAddress, "recipient must be safe address");
+    }
+
+
+    function sweepToken(
+        address token,
+        uint256 amountMinimum,
+        address recipient
+    ) external view onlySelf {
+
+        require(recipient == safeAddress, "recipient must be safe address");
+        require(tokenWhiteList[token], "token is not allowed");
+    }
+
+        
 }
--- contracts/acl/uniswapACL/arbitrum/ArbitrumSwapRouterAccessControl.sol
+++ contracts/acl/uniswapACL/arbitrum/ArbitrumSwapRouterAccessControl.sol
@@ -2,22 +2,126 @@
 
 pragma solidity ^0.8.4;
 
-import "../base/SwapRouterAccessControl.sol";
+import "../../../interfaces/uniswap/ISwapRouter.sol";
 
 
-contract ArbitrumSwapRouterAccessControl is SwapRouterAccessControl {
+contract ArbitrumSwapRouterAccessControl {
+
+    /// @dev The length of the bytes encoded address
+    uint256 private constant ADDR_SIZE = 20;
+    /// @dev The length of the bytes encoded fee
+    uint256 private constant FEE_SIZE = 3;
+
+    /// @dev The offset of a single token address and pool fee
+    uint256 private constant NEXT_OFFSET = ADDR_SIZE + FEE_SIZE;
+    /// @dev The offset of an encoded pool key
+    uint256 private constant ONE_POOL_LENGTH = NEXT_OFFSET + ADDR_SIZE;
+
+    address public safeAddress;
+    address public safeModule;
+    address public uniSwapRouter;
+    
+    mapping(address=>bool) public tokenWhiteList;
+
+    bytes32 internal _checkedRole;
 
     constructor(
         address _safeAddress,
         address _safeModule
-    ) SwapRouterAccessControl(
-        _safeAddress, 
-        _safeModule,
-        0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45 // swap router address
     ) {
+        require(_safeAddress != address(0), "invalid safe address");
+        require(_safeModule!= address(0), "invalid module address");
+        safeModule = _safeModule;
+        safeAddress = _safeAddress;
+        uniSwapRouter = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
         // usdc
         tokenWhiteList[0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8] = true;
         // weth
         tokenWhiteList[0x82aF49447D8a07e3bd95BD0d56f35241523fBab1] = true;
     }
+    modifier onlySelf() {
+        require(address(this) == msg.sender, "Caller is not inner");
+        _;
+    }
+
+    modifier onlyModule() {
+        require(safeModule == msg.sender, "Caller is not the module");
+        _;
+    }
+
+    function check(bytes32 _role, bytes calldata data) external onlyModule returns (bool) {
+        _checkedRole = _role;
+        (bool success,) = address(this).staticcall(data);
+        return success;
+    }
+
+    fallback() external {
+        revert("Unauthorized access");
+    }
+    
+    function exactInputSingle(ISwapRouter.ExactInputSingleParams calldata params) external view onlySelf {
+        require(tokenWhiteList[params.tokenIn], "tokenIn is not allowed");
+        require(tokenWhiteList[params.tokenOut], "tokenOut is not allowed");
+        require(params.recipient == safeAddress, "recipient must be safe address");
+        require(params.fee == 500, "feeTier must be 0.05%");
+    }
+
+    function toAddress(bytes memory _bytes, uint256 _start) internal pure returns (address) {
+        require(_start + 20 >= _start, 'toAddress_overflow');
+        require(_bytes.length >= _start + 20, 'toAddress_outOfBounds');
+        address tempAddress;
+
+        assembly {
+            tempAddress := div(mload(add(add(_bytes, 0x20), _start)), 0x1000000000000000000000000)
+        }
+
+        return tempAddress;
+    }
+
+    function toUint24(bytes memory _bytes, uint256 _start) internal pure returns (uint24) {
+        require(_start + 3 >= _start, 'toUint24_overflow');
+        require(_bytes.length >= _start + 3, 'toUint24_outOfBounds');
+        uint24 tempUint;
+
+        assembly {
+            tempUint := mload(add(add(_bytes, 0x3), _start))
+        }
+
+        return tempUint;
+    }
+
+    function exactInput(ISwapRouter.ExactInputParams calldata params) external view onlySelf { 
+        require(params.path.length == ONE_POOL_LENGTH, "only one pool");
+
+        address tokenA = toAddress(params.path, 0);
+        uint24 fee = toUint24(params.path, ADDR_SIZE);
+        address tokenB = toAddress(params.path, NEXT_OFFSET);
+
+        require(tokenWhiteList[tokenA], "tokenA is not allowed");
+        require(tokenWhiteList[tokenB], "tokenB is not allowed");
+        require(fee == 500, "feeTier must be 0.05%");
+        require(params.recipient == safeAddress, "recipient must be safe address");
+    }
+
+    function exactOutputSingle(ISwapRouter.ExactOutputSingleParams calldata params) external view onlySelf {
+        require(tokenWhiteList[params.tokenIn], "tokenIn is not allowed");
+        require(tokenWhiteList[params.tokenOut], "tokenOut is not allowed");
+        require(params.recipient == safeAddress, "recipient must be safe address");
+        require(params.fee == 500, "feeTier must be 0.05%");
+    }
+
+    function exactOutput(ISwapRouter.ExactOutputParams calldata params) external view onlySelf {
+
+        require(params.path.length == ONE_POOL_LENGTH, "only one pool");
+
+        address tokenA = toAddress(params.path, 0);
+        uint24 fee = toUint24(params.path, ADDR_SIZE);
+        address tokenB = toAddress(params.path, NEXT_OFFSET);
+
+        require(tokenWhiteList[tokenA], "tokenA is not allowed");
+        require(tokenWhiteList[tokenB], "tokenB is not allowed");
+        require(fee == 500, "feeTier must be 0.05%");
+        require(params.recipient == safeAddress, "recipient must be safe address");
+    }
+        
 }
