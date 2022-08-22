66d0e8510878d7105645c26d499f56e350443bf0
opcc <opcc22059@gmail.com>
2022-08-22 18:57:50 +0800

    update

---
+++ scripts/acl/deployArbitrumUniSwapRouterACL.js
@@ -0,0 +1,34 @@
+const hardhat = require("hardhat");
+const contracts = require("../deployed.js");
+
+// example
+// HARDHAT_NETWORK='izumiTest' node deployAirdrop.js
+// 
+
+const net = process.env.HARDHAT_NETWORK
+
+async function main() {
+    
+    const nftACL = await hardhat.ethers.getContractFactory("ArbitrumSwapRouterAccessControl");
+    const coboSafeAddress = contracts[net].coboSafeAddress
+    const coboSafeModule = contracts[net].coboSafeModule
+
+    const args = [
+      coboSafeAddress,
+      coboSafeModule
+    ]
+  
+    console.log("Deploying .....")
+    const nftACL = await nftACL.deploy(...args);
+    await nftACL.deployed();
+    console.log("nftACL address: " , nftACL.address);
+  
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  
