b1af68824bec8d30f6572526624c1eee76205ea1
opcc <opcc22059@gmail.com>
2023-06-08 18:58:43 +0800

    update

--- scripts/token/viewBalance.js
+++ scripts/token/viewBalance.js
@@ -1,38 +1,117 @@
-const hardhat = require("hardhat");
-const contracts = require("../deployed.js");
+const {ethers} = require("hardhat");
+
+const BigNumber = require('bignumber.js');
+
+const Web3 = require("web3");
+const secret = require('../../.secret.js');
+const pk = secret.pk;
+
+const config = require("../../hardhat.config.js");
+const contracts = require('../deployed.js');
+
+const tokenABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+]
 
 const v = process.argv
 const net = process.env.HARDHAT_NETWORK
 
+const rpc = config.networks[net].url
+var web3 = new Web3(new Web3.providers.HttpProvider(rpc));
 
-// Example: HARDHAT_NETWORK='izumiTest' node viewBalance 'BIT' '0xB96C85b716086a2410e874A1A6C8bDd4f35F2850'
+//Example: HARDHAT_NETWORK='bsc' node approveTokenWeb3.js testA 0x3a6d8ca21d1cf76f653a67577fa0d27453350dd8
 
 const para = {
-    symbol: v[2],
+    tokenSymbol: v[2],
+    tokenAddress: contracts[net][v[2]],
     address: v[3],
-}
-async function attachToken(address) {
-    var contractFactory = await hardhat.ethers.getContractFactory("TestToken");
-    var contract = contractFactory.attach(address);
-    return contract;
 }
 
 async function main() {
-    if (para.symbol == 'ETH') {
-        const provider = hardhat.waffle.provider;
-        const balance0ETH = await provider.getBalance(para.address);
-        console.log('balance: ', balance0ETH.toString());
-    } else {
-        const tokenAddr = contracts[net][para.symbol];
-        const token = await attachToken(tokenAddr);
-        const balance = await token.balanceOf(para.address);
-        console.log('balance: ', balance.toString());
-    }
+
+
+  console.log("Paramters: ");
+  for ( var i in para) { console.log("    " + i + ": " + para[i]); }
+  
+  const [deployer] = await ethers.getSigners();
+  var token = new web3.eth.Contract(tokenABI, para.tokenAddress);
+
+  const balance = await token.methods.balanceOf(para.address).call()
+  
+  console.log('balance: ', balance)
+  
 }
 
-main()
-  .then(() => process.exit(0))
+main().then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
     process.exit(1);
-  });
+})
+
+module.exports = main;
