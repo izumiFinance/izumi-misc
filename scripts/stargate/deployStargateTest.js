a035240e35a8e78e0a0264a43f3eaee044cd97c6
opcc <opcc22059@gmail.com>
2023-12-15 16:34:42 +0800

    add sgeth in constructor

--- scripts/stargate/deployStargateTest.js
+++ scripts/stargate/deployStargateTest.js
@@ -2,13 +2,14 @@ const hardhat = require("hardhat");
 
 const v = process.argv
 const composer = v[2];
+const sgeth = v[3];
 
 async function main() {
     
     const StargateTest = await hardhat.ethers.getContractFactory("StargateTest");
   
     console.log("Deploying .....")
-    const stargateTest = await StargateTest.deploy(composer);
+    const stargateTest = await StargateTest.deploy(composer, sgeth);
 
     console.log("stargateTest Contract Address: " , stargateTest.address);
   
