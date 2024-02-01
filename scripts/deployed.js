b2b369a43ae4fe68eca29466e69d74e571a755bd
opcc <opcc22059@gmail.com>
2024-02-01 14:41:07 +0800

    add tokens on zeta

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -290,6 +290,12 @@ const contracts = {
     },
 	zeta: {
 	    iZiSwapFactory: '0x8c7d3063579BdB0b90997e18A770eaE32E1eBb08',
+        USDT_bsc: '0x91d4F0D54090Df2D81e834c3c8CE71C6c865e79F',
+        ETH: '0xd97B1de3619ed2c6BEb3860147E30cA8A7dC9891',
+        stZETA: '0x45334a5b0a01ce6c260f2b570ec941c680ea62c0',
+        ZETA: '0x5f0b1a82749cb4e2278ec87f8bf6b618dc71a8bf',
+        USDT_eth: '0x7c8dDa80bbBE1254a7aACf3219EBe1481c6E01d7',
+        iZi: '0x60d01ec2d5e98ac51c8b4cf84dfcce98d527c747',
 	},
 }
 
